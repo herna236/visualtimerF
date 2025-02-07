85d8230394822760c4148a0ac19107123fb8037c
Aaron <Herna236@gmail.com>
2025-02-07 12:17:15 -0500

    added scripts

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/register': 'https://backend1-q4cw.onrender.com', // Proxy the /register API to the backend
-    }
-  }
-});
+})
